be40c1763a9e6faae7527a403a77a80c99db0e70
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-24 18:10:37 -0300

    adicionando propriedades à entidade user

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,6 +1,15 @@
 export class UserEntity {
     private readonly id: string;
-    private userCame: string;
+    private username: string;
     private fullName: string;
     private email: string;
+    private cpf: string;
+    private phone: string;
+    private address: string;
+    private state: string;
+    private city: string;
+    private cep: string;
+    private photo: string;
+    private roleId: number;
+    private ads: Array<any>;
 }
