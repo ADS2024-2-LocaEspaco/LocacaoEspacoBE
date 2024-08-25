875a4dbac2f796b748d2baeb0e8783a4bd6fce7d
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-25 12:49:19 -0300

    métodos get - entidade user

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,15 +1,74 @@
 export class UserEntity {
-    private readonly id: string;
-    private username: string;
-    private fullName: string;
-    private email: string;
-    private cpf: string;
-    private phone: string;
-    private address: string;
-    private state: string;
-    private city: string;
-    private cep: string;
-    private photo: string;
-    private roleId: number;
-    private ads: Array<any>;
+  private readonly id: string;
+  private username: string;
+  private fullName: string;
+  private email: string;
+  private cpf: string;
+  private phone: string;
+  private address: string;
+  private state: string;
+  private city: string;
+  private cep: string;
+  private photo: string;
+  private roleId: number;
+  private ads: Array<any>;
+
+  //autenticação realizada com Google -> user será inicialmente criado com email e username
+  constructor(id: string, email: string, username: string) {
+    this.id = id;
+    this.email = email;
+    this.username = username;
+  }
+
+  get getId(): string {
+    return this.id;
+  }
+
+  get getUsername(): string {
+    return this.username;
+  }
+
+  get getFullname(): string {
+    return this.fullName;
+  }
+
+  get getEmail(): string {
+    return this.email;
+  }
+
+  get getCpf(): string {
+    return this.cpf;
+  }
+
+  get getPhone(): string {
+    return this.phone;
+  }
+
+  get getAddress(): string {
+    return this.address;
+  }
+
+  get getSteate(): string {
+    return this.state;
+  }
+
+  get getCity(): string {
+    return this.city;
+  }
+
+  get getCep(): string {
+    return this.cep;
+  }
+
+  get getPhoto(): string {
+    return this.photo;
+  }
+
+  get getRoleId(): number {
+    return this.roleId;
+  }
+
+  get getAds(): Array<any> {
+    return this.ads;
+  }
 }
