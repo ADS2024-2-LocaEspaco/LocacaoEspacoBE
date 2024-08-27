80c28e5c0962f37afaaa593ba5608a308fb654d2
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-08-26 21:59:05 -0300

    Create methods for interface

--- src/user/domain/repositories/user.repositories.ts
+++ src/user/domain/repositories/user.repositories.ts
@@ -6,4 +6,12 @@ export interface UserRepository {
     //Find user by attribute
     findUserbyId(id: string): Promise<UserEntity>;
     findUserbyEmail(email: string): Promise<UserEntity>;
+
+    //CRUD
+    createUser(id: string, email: string, username: string, photo: string): Promise<UserEntity>;
+    deleteUser(email: string): Promise<UserEntity>;
+    updateUser(user: UserEntity): Promise<UserEntity>;
+
+    // average classification
+    avgClassification(id: string): Number;
 }
