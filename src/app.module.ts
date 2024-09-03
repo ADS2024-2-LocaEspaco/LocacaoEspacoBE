62c69c5d3cdacf4f6266e3108c84f90b6b1f839c
Pedro Higor <pedro.higor92@gmail.com>
2024-09-03 15:55:40 -0300

    criação do shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/infrastructure/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
