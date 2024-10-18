ebac17262e9774a6a8516f1e106838fd73449b01
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-10-18 19:57:42 -0300

    correção do service de feedback

--- src/feedback/infrastructure/feedback.service.ts
+++ src/feedback/infrastructure/feedback.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { Feedback } from '@prisma/client';
-import { getComentariosAnuncio } from './repositories/Feedback.repositories';
 import { CreateFeedbackDto } from "./database/dto/create-feedback.dto";
+import { feedbackRepository } from './repositories/Feedback.repositories';
 
 @Injectable()
 export class FeedbackService {
+  constructor(
+    private readonly FeedbackRepository: feedbackRepository
+  ){}
   
-  async getComentarios(id: string): Promise<CreateFeedbackDto[] | any> {
+  async getComentarios(id: number): Promise<CreateFeedbackDto[] | string> {
     try {
-      return getComentariosAnuncio(id);
+      return this.FeedbackRepository.getComentariosAnuncio(id)
       
     } catch (error) {
-      return {"message": error};
+      return "nenhum comentario encontrado"
     }
   }
 }
