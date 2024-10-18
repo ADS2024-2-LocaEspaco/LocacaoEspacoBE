a12c86697285e78ccb5e9b9d3f0e348d0e618856
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-10-18 19:44:42 -0300

    corrigindo feedback repository e dto

--- src/feedback/infrastructure/repositories/Feedback.repositories.ts
+++ src/feedback/infrastructure/repositories/Feedback.repositories.ts
@@ -1,30 +1,45 @@
-import { Feedback, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
 import { CreateFeedbackDto } from "../database/dto/create-feedback.dto";
 
 const prisma = new PrismaClient();
 
-export async function getComentariosAnuncio(anuncioId: string): Promise<CreateFeedbackDto[]>{
-    const comentarios = prisma.feedback.findMany({
-        where:{
-            anuncioId
-        },
-        select:{
-            id: true,
-            descricao: true,
-            nota: true,
-            anuncioId: true,
-            userId: true,
-            createdAt: false,
-            user: {
-                select: {
-                    picture: true,
-                    firstName: true,
-                    lastName: true,
-                }
+@Injectable()
+export class feedbackRepository{
+    async getComentariosAnuncio(anuncioId: number): Promise<CreateFeedbackDto[] | string>{
+        const avaliacoes = await prisma.avaliacao.findMany({
+            where:{
+                reservas_id: anuncioId
+            },
+            select:{
+                id: true,
+                nota_limpeza: true,
+                nota_exatidao_anuncio: true,
+                nota_custo_beneficio: true,
+                nota_localizacao: true,
+                nota_seguiu_regras: true,
+                nota_pontualidade: true,
+                nota_cordialidade: true,
+                reservas_id: true,
+                comentario: true,
+                criado_em: false,
             }
-        }
-        
-    })
-    return comentarios
-}
+        })
 
+        const comentarios: CreateFeedbackDto[] = avaliacoes.map(avaliacao => ({
+            id: avaliacao.id,
+            nota_limpeza: avaliacao.nota_limpeza || null,
+            nota_exatidao_anuncio: avaliacao.nota_exatidao_anuncio || null,
+            nota_custo_beneficio: avaliacao.nota_custo_beneficio || null,
+            nota_localizacao: avaliacao.nota_localizacao || null,
+            nota_seguiu_regras: avaliacao.nota_seguiu_regras || null,
+            nota_pontualidade: avaliacao.nota_pontualidade || null,
+            nota_cordialidade: avaliacao.nota_cordialidade || null,
+            reservas_id: avaliacao.reservas_id,
+            comentario: avaliacao.comentario,
+        }));
+
+        // Retorna o array de comentários ou uma mensagem se não houver nenhum
+        return comentarios.length > 0 ? comentarios : "Nenhum comentário encontrado.";
+    }
+}
