0f6dcb16c48a11baf07499a77c1be4f5e44c28e9
GiovaniAppezzato <giovani.apztt@gmail.com>
2024-11-08 21:40:59 -0300

    feat: adicionado o seeder

---
+++ src/anuncio/infrastructure/database/seeder.sql
@@ -0,0 +1,17 @@
+USE stayeasy;
+
+INSERT INTO tipo_imovel (imovel)
+VALUES
+  ('Casa'),
+  ('Apartamento'),
+  ('Cabana'),
+  ('Conteiner'),
+  ('Fazenda'),
+  ('Casa ecológica'),
+  ('Casa de hóspedes'),
+  ('Casa de árvore'),
+  ('Tenda');
+  
+  
+  
+SELECT * FROM tipo_imovel;
